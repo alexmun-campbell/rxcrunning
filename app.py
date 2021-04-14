3f5f53e72e1c55ea22591f1e4ba9c9d465bd197c
alexmun-campbell <alexander.campbell23@gmail.com>
2021-04-14 21:31:27 +0100

    take 2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import os
 import csv
 
-
+# this is a test
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
